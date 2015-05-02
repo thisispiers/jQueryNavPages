8dbdcf5e321d19a3f7e6e7af1348454dc973b04c
Piers Simms <piers@c1h.co.uk>
2015-05-02 14:36:54 +0100

    1.0
    Include documentation & licensing

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
 # jQueryNavPages
+
+Animated, programmable and easily navigated pages.
+
+## Usage
+
+HTML:
+
+    <div id="nav-pages">
+        <div class="nav-page" id="nav-page-1">
+            Welcome to page 1 <a href="#nav-page-2">Go to page 2</a>
+        </div>
+        <div class="nav-page" id="nav-page-2">
+            Welcome to page 2 <a href="#" class="nav-page-back">Back</a>
+        </div>
+    </div>
+
+JS:
+
+`$('#nav-pages').navPages('.nav-page');` or `$('.nav-page').navPages();`
+
+CSS:
+
+You don't need to add any!
+
+## Options
+
+- **pages**<br>
+_Default: Selector_<br>
+_Required_<br>
+Set by jQuery selector `$(pages).navPages();` or by the first argument `$(parent).navPages(pages);`
+
+- **parent**<br>
+_Default: Selector parent_<br>
+_Required_<br>
+Set by jQuery selector `$(parent).navPages(pages);`. This element is used to wrap its contents in a `<div>` necessary for pages to render correctly.
+
+- **animation**<br>
+_Default: `{}`<br>
+_Optional_<br>
+This option is directly passed on to `$.animate()`. Please see [jQuery API docs](http://api.jquery.com/animate/#animate-properties-options) for more information
+
+- **startPage**<br>
+_Default: 0<br>
+_Optional_<br>
+0 based index of which page to show first
+
+- **backSelector**<br>
+_Default: `.nav-page-back`<br>
+_Optional_<br>
+When these elements are clicked, the page navigates in reverse.
+
+All options (except _pages_) can also be set by passing an object when calling `$.navPages();`
+
+## API
+
+- **goTo(`selector`)**<br>
+If an element with the `selector` is found inside the `parent` then it navigates to that page. Triggers `nav-page-nav` and `nav-page-goto`.
+
+- **goBack()**<br>
+Navigates back. Triggers `nav-page-nav` and `nav-page-back`.
+
+- **settings**<br>
+The settings used as an object, in case you ever need them.
+
+## Events
+
+- **nav-page-init**<br>
+_Arguments: `parent`_<br>
+Triggered once page markup is complete and events are bound
+
+- **nav-page-nav**<br>
+_Arguments: `new_page`, `old_page`_<br>
+Triggered when navigating back or navigating to a page
+
+- **nav-page-goto**<br>
+_Arguments: `new_page`, `old_page`_<br>
+Triggered when navigating to a page
+
+- **nav-page-back**<br>
+_Arguments: `new_page`, `old_page`_<br>
+Triggered when navigating back
---
+++ jquery.nav-pages.js
@@ -0,0 +1,103 @@
+/* jQueryNavPages 1.0 (https://github.com/thisispiers/jQueryNavPages)
+ * Animated, programmable and easily navigated pages
+ * Made available by Piers Simms (http://c1h.co.uk) under the MIT license (http://opensource.org/licenses/mit-license.php) */
+(function($){
+    $.fn.navPages = function(opts, opts2){
+        var settings = this.settings = {
+                pages: $(this),
+                parent: $(this).parent(),
+                animation: {}, // $.animate() defaults
+                startPage: 0,
+                backSelector: '.nav-page-back',
+            },
+            typeof_opts = typeof opts;
+        if(typeof_opts === 'string'){
+            settings.parent = $(this);
+            settings.pages = $(opts, settings.parent);
+            if(typeof opts2 === 'number'){
+                settings.startPage = opts2;
+            } else if(typeof opts2 === 'object'){
+                $.extend(settings, opts2);
+            }
+        } else if(typeof_opts === 'object'){
+            $.extend(settings, opts);
+        }
+
+        var pageHistory = this.pageHistory = [];
+
+        // init
+        if(!settings.parent.data('nav-pages-init')){
+            settings.parent.data('nav-pages-init', true).wrapInner('<div style="width:100%;height:100%;position:relative;overflow:hidden;"></div>'); // wrap pages
+
+            settings.pages.each(function(i){
+                var startDiff = $(this).index() - settings.startPage;
+                if(startDiff < 1){
+                    pageHistory.push($(this)); // push our start page into history
+                }
+                $(this).css({ // properly style each page
+                    'width': '100%',
+                    'height': '100%',
+                    'position': 'relative',
+                    'margin-right': '-100%',
+                    'float': 'left',
+                    'left': (startDiff * 100) + '%', // this means that (start - 1) = -100% left
+                });
+            });
+
+            settings.parent.trigger('nav-page-init', $(settings.parent));
+        }
+
+        // returned methods and properties
+        var _this = {
+            settings: settings,
+            goTo: function(e){
+                if(e.target){
+                    e.preventDefault();
+                    var $this = $(this),
+                        dataSelector = $this.data('goto-nav-page'),
+                        new_page_selector = (dataSelector ? dataSelector : $this.attr('href')),
+                        new_page = $(new_page_selector);
+                } else if(e){
+                    var new_page = $(e, settings.parent);
+                }
+                if(new_page && new_page.length == 1){
+                    pageHistory.push(new_page);
+                    var old_page = pageHistory[pageHistory.length - 2];
+
+                    // animate
+                    new_page.css({left: '100%'}).animate({left: 0}, settings.animation);
+                    old_page.css({left: 0}).animate({left: '-100%'}, settings.animation);
+                    var eventParams = [$(new_page), $(old_page)];
+                    settings.parent.trigger('nav-page-nav', eventParams);
+                    settings.parent.trigger('nav-page-goto', eventParams);
+                }
+            },
+            goBack: function(e){
+                if(e.target){
+                    e.preventDefault();
+                }
+                if(pageHistory.length > 1){
+                    var new_page = pageHistory[pageHistory.length - 2],
+                        old_page = pageHistory[pageHistory.length - 1];
+                    pageHistory.splice(pageHistory.length -1, 1);
+
+                    // animate
+                    new_page.css({left: '-100%'}).animate({left: 0}, settings.animation);
+                    old_page.css({left: 0}).animate({left: '100%'}, settings.animation);
+                    var eventParams = [$(new_page), $(old_page)];
+                    settings.parent.trigger('nav-page-nav', eventParams);
+                    settings.parent.trigger('nav-page-back', eventParams);
+                }
+            }
+        };
+
+        // bind events
+        var backSelector = settings.backSelector ? ', ' + settings.backSelector : '';
+        settings.parent.delegate('[href^=#][href!=#]:not([data-goback-nav-page]):not(' + settings.backSelector + '), [data-goto-nav-page]:not([data-goback-nav-page]):not(' + settings.backSelector + ')', 'click', _this.goTo);
+        settings.parent.delegate('[data-goback-nav-page]' + backSelector, 'click', _this.goBack);
+
+        // return
+        return _this;
+
+    };
+})(jQuery);
